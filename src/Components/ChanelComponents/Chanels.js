bb35e7744db18767e0784fc9962bd66339a863f1
eropkinaO <140596609+eropkinaO@users.noreply.github.com>
2024-02-06 20:56:05 +0300

    ninth commit

--- src/Components/ChanelComponents/Chanels.js
+++ src/Components/ChanelComponents/Chanels.js
@@ -1,23 +1,15 @@
 import Chanel from "./Chanel";
 import ChanelsData from "../../Data/ChanelsData";
 import React from "react";
-import { useEffect } from 'react';
 import { useSelector, useDispatch } from "react-redux";
 import { filterCategory, getSelectedCategory } from "../../redux/chanelSlice";
 
 const Chanels = () => {
    
     const selectedCategory = useSelector(getSelectedCategory);
-    const dispatch = useDispatch();
 
-
-    useEffect = ( () => {
-      dispatch(filterCategory('ALL'))
-    }, [dispatch])
 
     return(
-      
-      
         <div className="container">
             {ChanelsData
             .filter(chanel => {
